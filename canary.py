a76d39146eff44c3b14fd2f20ef4df03397c9527
Nick Brady <14315329+nick-brady@users.noreply.github.com>
2020-12-20 17:10:02 -0500

    tracking mac addrs working (#1)

--- canary.py
+++ canary.py
@@ -1,18 +1,31 @@
+import time
+from datetime import timedelta
+from pprint import pprint
+
 from scapy.all import sniff, Dot11ProbeReq
 
-def hp(pkt):
-     global packet
-     if not pkt.haslayer(Dot11ProbeReq):
-         return
-     print('is a probe!')
-     packet = pkt
-     if pkt.type == 0 and pkt.subtype == 4:
-         print('FOUND ONE!')
-         packet = pkt
+seen_devices = {}
+
+def handle_packet(pkt):
+    if not pkt.haslayer(Dot11ProbeReq):
+        return
+    if pkt.type == 0 and pkt.subtype == 4:
+        mac = pkt.addr2.upper()
+        now = time.time()
+        try:
+            last_seen = timedelta(
+                seconds=seen_devices[mac] - now
+            )
+            seen_devices[mac] = now
+            # if last_seen > timedelta(hours=1):
+                # play music, someone's home!
+        except KeyError:
+            seen_devices[mac] = now
+
+        pprint(seen_devices)
 
 def main():
-    print('hello world')
-    sniff(iface='en0', prn=hp)
+    sniff(iface='wlan1', prn=handle_packet)
 
 if __name__ == '__main__':
     main()
